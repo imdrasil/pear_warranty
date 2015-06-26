7fc27a1fb7082d5f1d50bc9ffeea9773cf170a44
Roman <mo-ran@mail.ru>
2015-06-26 18:18:24 +0300

    Fix problem with incorrect imei number

--- lib/pear_warranty.rb
+++ lib/pear_warranty.rb
@@ -30,9 +30,10 @@ module PearWarranty
     if error
       {error: 'Problem with proxy'}
     else
-      if page =~ /but this serial number is not valid/
+      if page =~ /(but this serial number is not valid)|(but the serial number you have provided cannot be found in our records)/
         { error: 'There is no such imei or service is not available at this moment' }
       else
+        puts page.split('warrantyPage.warrantycheck.displayPHSupportInfo')
         text = page.split('warrantyPage.warrantycheck.displayPHSupportInfo')[1].scan(/\(.*\)/)[0].delete('()')
         params = text.split(', ')
         warranty = to_boolean(params.first.delete ' ')
--- lib/pear_warranty/version.rb
+++ lib/pear_warranty/version.rb
@@ -1,3 +1,3 @@
 module PearWarranty
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
